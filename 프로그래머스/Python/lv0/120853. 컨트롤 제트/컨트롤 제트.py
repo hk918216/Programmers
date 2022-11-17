288cdc34182eb147b5e35dc2c64df33e0a28d094
HYEGYEONG SEONG <71112121+hk918216@users.noreply.github.com>
2022-11-17 14:21:51 +0900

    Update and rename 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py to 프로그래머스/Python/lv0/120853. 컨트롤 제트/컨트롤 제트.py

--- 프로그래머스/Python/lv0/120853. 컨트롤 제트/컨트롤 제트.py
+++ 프로그래머스/Python/lv0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -2,7 +2,7 @@ def solution(s):
     s = s.split(" ")
     answer = []
     for i in s:
-        if i == 'Z':
+        if i == 'Z':  # or if i == 'Z' and answer:
             answer.pop()
         else:
             answer.append(int(i))
