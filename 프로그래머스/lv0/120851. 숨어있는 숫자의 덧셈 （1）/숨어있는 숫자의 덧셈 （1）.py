6ae9011f65e0b13f19224894e82ef41973e092fd
HYEGYEONG SEONG <71112121+hk918216@users.noreply.github.com>
2022-11-18 12:07:24 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -2,3 +2,6 @@ import re
 
 def solution(my_string):
     return sum(map(int, list(re.sub('[^0-9]',"",my_string))))
+
+def solution(my_string):
+    return sum(int(s) for s in my_string if s.isdigit())
