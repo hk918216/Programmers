a1b28b696d4010f2d1c8b8118957c47c110bc82d
HYEGYEONG SEONG <71112121+hk918216@users.noreply.github.com>
2022-11-19 15:14:07 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.15 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -0,0 +1,10 @@
+import re
+
+def solution(my_string):
+    answer = ''
+    for i in my_string:
+        if i.islower() == True:
+            answer += i.upper()
+        else:
+            answer += i.lower()
+    return answer
