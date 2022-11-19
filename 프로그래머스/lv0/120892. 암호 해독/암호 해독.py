8f74cfd4760a6cdb8cf98ea3d4150a813b6fd280
HYEGYEONG SEONG <71112121+hk918216@users.noreply.github.com>
2022-11-19 15:06:18 +0900

    [level 0] Title: 암호 해독, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
@@ -1,3 +1,10 @@
 def solution(cipher, code):
     
     return cipher[code-1::code]
+
+def solution(cipher, code):
+    answer = ''
+    for i in range(len(cipher)):
+        if (i+1) % code == 0:
+            answer += cipher[i]
+    return answer
