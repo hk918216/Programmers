3b34f901d6db3b2c0d815b065d4c96ebc5b084ec
HYEGYEONG SEONG <71112121+hk918216@users.noreply.github.com>
2022-11-22 20:16:28 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,3 +1,4 @@
+#1
 def solution(my_string):
     my_string = my_string.split()
     answer = int(my_string[0])
@@ -7,3 +8,6 @@ def solution(my_string):
         else:
             answer -= int(my_string[i+1])
     return answer
+
+#2
+solution = eval # 매개변수로 받는 식을 문자열로 받아서 실행하는 함수
