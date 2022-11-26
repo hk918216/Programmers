bf8325692d728f9ceed70501cc9f074cfba78870
HYEGYEONG SEONG <71112121+hk918216@users.noreply.github.com>
2022-11-26 17:16:23 +0900

    Rename 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py to 프로그래머스/Python/lv0/120864. 숨어있는 숫자의 덧셈(2)/숨어있는 숫자의 덧셈(2).py

---
+++ 프로그래머스/Python/lv0/120864. 숨어있는 숫자의 덧셈(2)/숨어있는 숫자의 덧셈(2).py
@@ -0,0 +1,4 @@
+import re
+
+def solution(my_string):
+    return sum([int(i) for i in re.sub('[^0-9]','-', my_string).split('-') if i ])
--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++
@@ -1,4 +0,0 @@
-import re
-
-def solution(my_string):
-    return sum([int(i) for i in re.sub('[^0-9]','-', my_string).split('-') if i ])
