2b277a10cb52b6f33f316ff910680dd19e6753c7
HYEGYEONG SEONG <71112121+hk918216@users.noreply.github.com>
2022-11-28 16:33:41 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -1,3 +1,4 @@
+#1
 def solution(lines):
     answer = [set([]) for _ in range(200)]
     for index, line in enumerate(lines):
@@ -10,3 +11,8 @@ def solution(lines):
         if len(line) > 1:
             count += 1
     return count
+
+#2
+def solution(lines):
+    sets = [set(range(min(i), max(i))) for i in lines]
+    return len(sets[0] & sets[1] | sets[0] & sets[2] | sets[1] & sets[2])
