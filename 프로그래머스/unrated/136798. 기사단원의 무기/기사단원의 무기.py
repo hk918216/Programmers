c8609151ff4d6ff5bbcdbfc43855bb431471b9dd
HYEGYEONG SEONG <71112121+hk918216@users.noreply.github.com>
2022-12-07 17:08:14 +0900

    [unrated] Title: 기사단원의 무기, Time: 3.81 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,17 @@
+def divisor(n):
+    a = 0
+    for i in range(1, int(n**(1/2)+1)):
+        if n % i == 0:
+            a += 1
+            if i**2 != n:
+                a += 1
+    return a
+
+def solution(number, limit, power):
+    answer = 0
+    for i in range(1,number+1):
+        if divisor(i) > limit:
+            answer += power
+        else:
+            answer += divisor(i)
+    return answer
