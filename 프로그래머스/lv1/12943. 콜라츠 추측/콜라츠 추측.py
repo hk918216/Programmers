c3a13997a24751a32ec3bec940ebe5994dd561fc
HYEGYEONG SEONG <71112121+hk918216@users.noreply.github.com>
2022-12-09 19:04:09 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -6,7 +6,7 @@ def solution(num):
         if answer == 500:
             return -1
         if num % 2 == 0:
-            num = num // 2
+            num = num / 2
         else :
             num = num * 3 + 1
         answer += 1
