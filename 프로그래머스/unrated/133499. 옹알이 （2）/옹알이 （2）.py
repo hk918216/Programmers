f333877370a328ec85294025320c20ddc428eaae
HYEGYEONG SEONG <71112121+hk918216@users.noreply.github.com>
2023-01-03 13:38:19 +0900

    [unrated] Title: 옹알이 (2), Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -0,0 +1,10 @@
+def solution(babbling):
+    answer = 0
+    bab = ['aya','ye','woo','ma']
+    for i in babbling:
+        for j in bab:
+            if j*2 not in i:
+                i = i.replace(j, '*')
+        if i.strip('*')=='':
+            answer += 1
+    return answer
