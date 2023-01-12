da41409f1bd4f6bb590521bb78b8194ac704c962
HYEGYEONG SEONG <71112121+hk918216@users.noreply.github.com>
2023-01-12 14:31:24 +0900

    [level 2] Title: 행렬의 곱셈, Time: 0.55 ms, Memory: 28.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -5,3 +5,6 @@ def solution(arr1, arr2):
     arr2 = np.array(arr2)
     answer = arr1.dot(arr2)
     return answer.tolist()
+
+def solution2(arr1, arr2):
+    return (np.matrix(arr1)*np.matrix(arr2)).tolist()
